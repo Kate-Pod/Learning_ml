bb75114fca511db67f22e67d26f76f10f586dc86
Kate-Pod <75089953+Kate-Pod@users.noreply.github.com>
2021-03-18 20:53:02 +0300

    Update Decision_tree.py

--- Habr/Decision_tree.py
+++ Habr/Decision_tree.py
@@ -1,1 +1,10 @@
+#Сгенерируем данные
 
+# первый класс
+np.seed = 7
+train_data = np.random.normal(size=(100, 2))
+train_labels = np.zeros(100)
+
+# добавляем второй класс
+train_data = np.r_[train_data, np.random.normal(size=(100, 2), loc=2)]
+train_labels = np.r_[train_labels, np.ones(100)]
