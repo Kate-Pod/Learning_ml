ce2c85eb335329327fafc109a7d51a92493ff2ac
Kate-Pod <75089953+Kate-Pod@users.noreply.github.com>
2021-03-18 23:28:29 +0300

    Update Decision_tree.py

--- Habr/Decision_tree.py
+++ Habr/Decision_tree.py
@@ -8,3 +8,54 @@ train_labels = np.zeros(100)
 # добавляем второй класс
 train_data = np.r_[train_data, np.random.normal(size=(100, 2), loc=2)]
 train_labels = np.r_[train_labels, np.ones(100)]
+
+#Нарисуем график классификации двух переменных с границей
+plt.rcParams['figure.figsize'] = (10,8)
+plt.scatter(train_data[:, 0], train_data[:, 1], c=train_labels, s=100, 
+cmap='autumn', edgecolors='black', linewidth=1.5);
+plt.plot(range(-2,5), range(4,-3,-1));
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+# Напишем вспомогательную функцию, которая будет возвращать решетку для дальнейшей визуализации.
+def get_grid(data):
+    x_min, x_max = data[:, 0].min() - 1, data[:, 0].max() + 1
+    y_min, y_max = data[:, 1].min() - 1, data[:, 1].max() + 1
+    return np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+
+# параметр min_samples_leaf указывает, при каком минимальном количестве
+# элементов в узле он будет дальше разделяться
+clf_tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=17)
+
+# обучаем дерево
+clf_tree.fit(train_data, train_labels)
+
+# немного кода для отображения разделяющей поверхности
+xx, yy = get_grid(train_data)
+predicted = clf_tree.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
+plt.pcolormesh(xx, yy, predicted, cmap='autumn')
+plt.scatter(train_data[:, 0], train_data[:, 1], c=train_labels, s=100, 
+cmap='autumn', edgecolors='black', linewidth=1.5);
+
+# используем .dot формат для визуализации дерева
+from sklearn.tree import export_graphviz
+export_graphviz(clf_tree, feature_names=['x1', 'x2'], 
+out_file='../../img/small_tree.dot', filled=True)
+# для этого понадобится библиотека pydot (pip install pydot)
+!dot -Tpng '../../img/small_tree.dot' -o '../../img/small_tree.png'
+
+
+
+#Дерево решений с количественными переменными
+data=pd.DataFrame({'Возраст': [17,64,18,20,38,49,55,25,29,21,33],'Невозврат кредита':[1,0,1,0,1,0,0,1,1,0,1]})
+
+age_tree = DecisionTreeClassifier(random_state=17)
+age_tree.fit(data['Возраст'].values.reshape(-1, 1), data['Невозврат кредита'].values)
+
+export_graphviz(age_tree, feature_names=['Возраст'], 
+out_file='../../img/age_tree.dot', filled=True)
+!dot -Tpng '../../img/age_tree.dot' -o '../../img/age_tree.png'
+
+
+
